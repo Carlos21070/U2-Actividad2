2c4b2eeb21abe044ef859a90a7235ae82abc6dcf
Carlos Alejandro Ramos Hernández <carlos.ramos99@hotmail.com>
2020-09-18 13:28:17 -0500

    Version 1

---
+++ U2_Actividad2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using U2_Actividad2.Models;
+
+namespace U2_Practica3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            villancicosContext context = new villancicosContext();
+            var compositores = context.Villancico.OrderByDescending(x => x.Anyo);
+            return View(compositores);
+        }
+        public IActionResult Villancico(int? id)
+        {
+            
+            if (id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            villancicosContext context = new villancicosContext();
+            var compositores = context.Villancico.FirstOrDefault(x => x.Id==id);
+            if (compositores==null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+              return View(compositores);
+            }
+            
+        }
+    }
+}
